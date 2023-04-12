0a07bc8da8f55a783ce9af5134cbadd13e451492
linosk <linakoskar@gmail.com>
2023-04-12 18:36:33 +0200

    Progress

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-#include "png_chunk.h"
+#include "png_place_holder.h"
 
 int main(int argc, char** argv){
+
+    if (argc!=2){
+        std::cout<<"Run ./png_modifier [file.png]"<<std::endl;
+        exit(0);
+    }
+
+    PNG_Place_Holder PNG;
+    PNG.PNG_Get_Bytes(argv[1]);
 
     return 0;
 }
--- png_place_holder.h
+++ png_place_holder.h
@@ -1,9 +1,15 @@
 #ifndef __PNG_PLACE_HOLDER__
 #define __PNG_PLACE_HOLDER__
 
-//This class will consist of every byte of into .png file
-class png_place_holder{
+#include <string>
 
+#include "png_chunk.h"
+
+//This class will consist of every byte of into .png file
+class PNG_Place_Holder{
+    public:
+        PNG_Byte* PNG_File;
+        void PNG_Get_Bytes(std::string File_name);
 };
 
 #endif //__PNG_PLACE_HOLDER__
