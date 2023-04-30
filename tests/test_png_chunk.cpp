e2740364a016cb433d3c61608c390cf22b1b503a
linosk <linakoskar@gmail.com>
2023-04-30 20:48:49 +0200

    Pain

---
+++ tests/test_png_chunk.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include "include/png_chunk.h"
+
+TEST(Test_PNG_copy,test){
+    PNG_byte Array_one[CHUNK_SIZE];
+    PNG_byte Array_two[CHUNK_SIZE] = {'a','b','c','d'};
+    PNG_byte Array_three[CHUNK_SIZE];
+    PNG_byte Array_four[2*CHUNK_SIZE] = {0,0,0,0,97,98,99,100};
+
+    PNG_copy(Array_two,0,CHUNK_SIZE,Array_one);
+    PNG_copy(Array_four,CHUNK_SIZE,CHUNK_SIZE,Array_three);
+
+    EXPECT_TRUE(PNG_compare(Array_two,0,CHUNK_SIZE,Array_one));
+    EXPECT_TRUE(PNG_compare(Array_four,CHUNK_SIZE,CHUNK_SIZE,Array_three));
+}
