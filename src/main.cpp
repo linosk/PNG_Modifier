7713e5f710cff3cc15b09af98f8dcc32b8c911ed
linosk <linakoskar@gmail.com>
2023-05-05 16:41:03 +0200

    What to fix

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,13 @@ int main(int argc, char** argv){
         exit(0);
     }
 
+    /*
+    Program received signal SIGSEGV, Segmentation fault.
+    0x0000555555558755 in PNG_copy (Copy=std::vector of length 176, capacity 176 = {...}, Beg=8 '\b', Range=4 '\004', Copy_to=std::vector of length 0, capacity 0)
+    at /home/me/Projects/PNG_Modifier/src/png_chunk.cpp:56
+    (gdb) n
+    */
+
     PNG_place_holder PNG;
     PNG.PNG_get_bytes(argv[1]);
     PNG.IHDR.PNG_get_chunk(PNG.PNG_file, PNG.IHDR_header, "IHDR", PNG.Pointer);
