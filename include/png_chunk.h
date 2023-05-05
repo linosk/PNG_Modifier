f6a4503d45a93dd1111cc68a5f9f7ecdfcb57b38
linosk <linakoskar@gmail.com>
2023-05-05 16:30:22 +0200

    Segmentation core dumped when reading from PNG file

--- include/png_chunk.h
+++ include/png_chunk.h
@@ -1,7 +1,6 @@
 #ifndef __PNG_CHUNK__
 #define __PNG_CHUNK__
 
-#include <array>
 #include <vector>
 #include <iostream>
 #include <cstdint>
@@ -19,24 +18,25 @@ class PNG_chunk{
         //PNG_byte Type_bytes[CHUNK_SIZE];
         //PNG_byte* Chunk_data;
         //PNG_byte CRC_bytes[CHUNK_SIZE];
-        std::array<PNG_byte,CHUNK_SIZE> Data_length_bytes{};
+        std::vector<PNG_byte> Data_length_bytes{};
         uint32_t Data_length;
-        std::array<PNG_byte,CHUNK_SIZE> Type_bytes{};
+        std::vector<PNG_byte> Type_bytes{};
         std::vector<PNG_byte> Chunk_data{};
-        std::array<PNG_byte,CHUNK_SIZE> CRC_bytes{};
+        std::vector<PNG_byte> CRC_bytes{};
     public:
-        void PNG_get_chunk(PNG_byte* PNG_file, PNG_byte* PNG_chunk_header, std::string PNG_header_name, size_t &Counter);
+        //void PNG_get_chunk(PNG_byte* PNG_file, PNG_byte* PNG_chunk_header, std::string PNG_header_name, size_t &Counter);
+        void PNG_get_chunk(std::vector<PNG_byte> PNG_file, const std::vector<PNG_byte> PNG_chunk_header, std::string PNG_header_name, size_t &Counter);
 };
 
 //void PNG_print(PNG_byte* Array, uint8_t Size);
-template <size_t N>
-void PNG_print(const std::array,<PNG_byte, N> &Array);
+void PNG_print(const std::vector<PNG_byte> Array);
 
-bool PNG_compare(PNG_byte* Compare, uint8_t Beg, uint8_t Range, PNG_byte* Compare_to);
+//bool PNG_compare(PNG_byte* Compare, uint8_t Beg, uint8_t Range, PNG_byte* Compare_to);
+bool PNG_compare(const std::vector<PNG_byte> Compare, uint8_t Beg, uint8_t Range, const std::vector<PNG_byte> Compare_to);
 
 //Copy from one PNG_byte array to the other in given range
-void PNG_copy(PNG_byte* Copy, uint8_t Beg, uint8_t Range, PNG_byte* Copy_to);
+void PNG_copy(const std::vector<PNG_byte> Copy, uint8_t Beg, uint8_t Range, std::vector<PNG_byte> Copy_to);
 
-u_int32_t PNG_sum_chunks(PNG_byte* Data_length_bytes, int Length);
+u_int32_t PNG_sum_chunks(const std::vector<PNG_byte> Data_length_bytes);
 
 #endif
--- include/png_place_holder.h
+++ include/png_place_holder.h
@@ -9,9 +9,16 @@ class PNG_place_holder{
 
     public:
         //Maybe change it later to private
-        PNG_byte Correct_signature[BYTE_SIZE] = {137,80,78,71,13,10,26,10};
-        PNG_byte IHDR_header[BYTE_SIZE] = {'I','H','D','R'};
-        PNG_byte* PNG_file;
+        //PNG_byte Correct_signature[BYTE_SIZE] = {137,80,78,71,13,10,26,10};
+        //PNG_byte IHDR_header[BYTE_SIZE] = {'I','H','D','R'};
+        //PNG_byte* PNG_file;
+        //size_t Pointer;
+        //void PNG_get_bytes(std::string File_name);
+        //PNG_chunk IHDR;
+
+        std::vector<PNG_byte> Correct_signature = {137,80,78,71,13,10,26,10};
+        std::vector<PNG_byte> IHDR_header = {'I','H','D','R'};
+        std::vector<PNG_byte> PNG_file{};
         size_t Pointer;
         void PNG_get_bytes(std::string File_name);
         PNG_chunk IHDR;
