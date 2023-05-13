6f157aa89a13ca009e3f3c53e4754e31ec1147d1
linosk <linakoskar@gmail.com>
2023-05-13 22:08:19 +0200

    Cleaned

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,5 @@ int main(int argc, char** argv){
     PNG.PNG_get_bytes(argv[1]);
     PNG.IHDR.PNG_get_chunk(PNG.PNG_file, PNG.IHDR_header, "IHDR", PNG.Pointer);
 
-    crc::Bin_arr A = {1,0,1};
-    crc::Print_binary(A);
-
-    crc::Bin_arr Binary_three{};
-    crc::Bin_arr Binary_three_compare = {0,0,0,0,1,1,1,1,0,0,0,1,0,0,0,0};
-    crc::To_binary(2288,1,Binary_three);
-
-    crc::Print_binary(Binary_three);
-    crc::Print_binary(Binary_three_compare);
-
     return 0;
 }
