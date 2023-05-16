5f24cc17ef2956fddddcf9f24fdfc565441b9339
linosk <linakoskar@gmail.com>
2023-05-16 18:32:54 +0200

    Read comments in CRC_check

--- src/png_place_holder.cpp
+++ src/png_place_holder.cpp
@@ -54,18 +54,50 @@ bool PNG_place_holder::Check_CRC(const PNG_array Type_bytes, const PNG_array Chu
     crc::Bin_arr Binary{};
     Binary.resize(Data_length*BYTE_SIZE);
     for(int i =0, j=0;i<Data_length*BYTE_SIZE;i+=BYTE_SIZE,j++){
-        std::cout<<static_cast<unsigned int>(Data[i])<<"\n";
         crc::To_binary(Data[j],i,Binary);
     }
 
-    crc::Print_binary(Binary);
-    crc::Reverse_binary(Binary);
-    crc::Add_zeros(Binary, 4*BYTE_SIZE);
-    crc::Reverse_binary(Binary);
-    crc::Flip_binary(Binary,0);
-    crc::Bin_arr Rem = crc::CRC(Data,Polyniomal);
-    crc::Flip_binary(Binary,0);
+    std::cout<<"----------------------------"<<"\n";
 
-    //Flip_binary has to modified, take look at main.cpp in XD directory, make test for this function and fix test for test_binary
+    crc::Reverse_binary(Binary);
+    crc::Print_binary(Binary);
+
+std::cout<<"----------------------------"<<"\n";
+
+
+    crc::Add_zeros(Binary,4*BYTE_SIZE);
+    crc::Reverse_binary(Binary);
+    crc::Print_binary(Binary);
+
+std::cout<<"----------------------------"<<"\n";
+
+
+    crc::Flip_binary(Binary,0,4*BYTE_SIZE);
+    crc::Print_binary(Binary);
+
+std::cout<<"----------------------------"<<"\n";
+
+    crc::Bin_arr Rem = crc::CRC(Binary,Polyniomal);
+    crc::Print_binary(Rem);
+    crc::Flip_binary(Rem,0,4*BYTE_SIZE);
+    crc::Print_binary(Rem);
+
+    crc::Reverse_binary(Rem);
+    crc::Print_binary(Rem);
+    crc::Reverse_binary(Rem);
+
+    PNG_array CRC_compare{};
+    CRC_compare.resize(CHUNK_SIZE);
+
+    for(int i =0, j=0;i<Data_length*BYTE_SIZE;i+=BYTE_SIZE,j++){
+        crc::To_binary(Data[j],i,Binary);
+        CRC_compare[j] = crc::To_decimal(i,Rem);
+    }
+
+    PNG_print(CRC_compare);
+
+    //Flip_binary has to modified, take look at main.cpp in XD directory, make test for this function and fix test for test_binary - MOSTLY DONE
+    //Tests need to be corrected
+    //SOMETHING WRON
 
 }
